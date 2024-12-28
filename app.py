9289d226afcb344129cf803df16e50f0a5abcf03
JhPorto29 <joaohenriqueporto19@gmail.com>
2024-12-28 12:34:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,10 @@
-from dotenv import load_dotenv
-import os
-import psycopg2
-from psycopg2 import sql
+from flask import Flask, render_template
 
-# Carregar variáveis de ambiente
-load_dotenv()
+app = Flask(__name__)
 
-# Obter URL do banco de dados a partir da variável de ambiente
-DATABASE_URL = os.getenv('DATABASE_URL')
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-# Verificar se a variável de ambiente foi carregada corretamente
-if DATABASE_URL is None:
-    raise ValueError("A variável de ambiente DATABASE_URL não está configurada.")
-
-try:
-    # Conectar ao banco de dados PostgreSQL
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-    # Criar um cursor
-    cur = conn.cursor()
-
-    # Executar uma consulta SQL segura
-    query = sql.SQL("SELECT * FROM {}").format(sql.Identifier('tabela'))  # Substitua 'tabela' pelo nome real da tabela
-    cur.execute(query)
-
-    # Obter os resultados
-    rows = cur.fetchall()
-    print(rows)
-
-except Exception as e:
-    print(f"Erro ao conectar ao banco de dados: {e}")
-
-finally:
-    # Fechar o cursor e a conexão
-    if cur:
-        cur.close()
-    if conn:
-        conn.close()
+if __name__ == '__main__':
+    app.run(debug=True)
